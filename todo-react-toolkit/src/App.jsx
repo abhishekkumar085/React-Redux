719a1df79e67b03cbdf9daefa15fd807f3f7523f
abhishekkumar085 <abhishek950820@gmail.com>
2024-11-27 17:53:50 +0530

    Added Api Call Via AsyncThunk

--- todo-react-toolkit/src/App.jsx
+++ todo-react-toolkit/src/App.jsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import TodoInput from './Components/TodoInput';
 import TodoList from './Components/TodoList';
+import { fetchUsers } from './redux/userSlice';
 
 const App = () => {
   const todos = useSelector((state) => state.todo.todos);
   const [editId, setEditId] = useState(null); // Track the todo being edited
   const [editText, setEditText] = useState(''); // Track the text to edit
 
+  const dispatch=useDispatch();
+  const {users,status,error}=useSelector((state)=>state.user);
+
+  console.log(users,status,error);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+  
+
   return (
-    <div className="max-w-lg mx-auto p-4 bg-gray-50 min-h-screen">
+    <div className="max-w-lg mx-auto p-4 bg-gray-50 min-h-full">
       <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
         Todo - Redux Toolkit
       </h1>
@@ -20,6 +31,10 @@ const App = () => {
         setEditId={setEditId}
       />
       <TodoList todos={todos} setEditId={setEditId} setEditText={setEditText} />
+
+      <div>
+
+      </div>
     </div>
   );
 };
--- todo-react-toolkit/src/redux/store.js
+++ todo-react-toolkit/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import todoReducer from '../redux/todoSlice'
+import todoReducer from '../redux/todoSlice';
+import userReducer from '../redux/userSlice';
 
 const store = configureStore({
   reducer: {
-    todo:todoReducer
+    todo: todoReducer,
+    user: userReducer,
   },
 });
 export default store;
